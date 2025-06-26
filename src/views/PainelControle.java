abcd67e73ecee77f050f206d5da983f44e61f643
Beatriz Camilo <abeatrizcamilo@gmail.com>
2025-06-26 20:32:08 -0300

    Atualizando a documentação para a classe

--- src/views/PainelControle.java
+++ src/views/PainelControle.java
@@ -1,5 +1,32 @@
 package views;
 
+import repositories.RepositorioClientes;
+import repositories.RepositorioFilmes;
+import repositories.RepositorioFuncionarios;
+import repositories.RepositorioSalas;
+import repositories.RepositorioSessao;
+
 public class PainelControle {
+
+    private  RepositorioSessao repositorioSessoes;
+    private  RepositorioClientes repositorioClientes;
+    private  RepositorioFilmes repositorioFilmes;
+    private  RepositorioFuncionarios repositorioFuncionario;
+    private  RepositorioSalas repositorioSalas;
+
+    public PainelControle(RepositorioSessao repositorioSessoes, RepositorioClientes repositorioClientes,
+                          RepositorioFilmes repositorioFilmes, RepositorioFuncionarios repositorioFuncionario,
+                          RepositorioSalas repositorioSalas) {
+        this.repositorioSessoes = repositorioSessoes;
+        this.repositorioClientes = repositorioClientes;
+        this.repositorioFilmes = repositorioFilmes;
+        this.repositorioFuncionario = repositorioFuncionario;
+        this.repositorioSalas = repositorioSalas;
+    }
+
+    public void menuTipoUsuario(){
+        
+    }
+
     
 }
